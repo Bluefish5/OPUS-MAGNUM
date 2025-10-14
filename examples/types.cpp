9bb55cb5fa5f3bb7d6f51ef6e3fff0fff956ef62
Rafal <60304472+Bluefish5@users.noreply.github.com>
2025-10-14 23:23:13 +0200

    Int types

---
+++ examples/types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a;
+    extern int b;
+
+    short int c;
+    int d;
+    long int e;
+    long long int f;
+
+    signed int g;
+    unsigned int h;
+
+    
+
+    return 0;
+
+}
